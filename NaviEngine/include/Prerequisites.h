c3382048230a0ca3e7fb4ee5ab4c4e3e50b4b017
JSebasti16 <gbojesbastianc@gmail.com>
2025-09-12 09:28:26 -0600

    DeviceContext

--- NaviEngine/include/Prerequisites.h
+++ NaviEngine/include/Prerequisites.h
@@ -39,3 +39,25 @@
         OutputDebugStringW(L"Failed to log error message.\n");\
     }                                                         \
 }
+
+struct SimpleVertex
+{
+  XMFLOAT3 Pos;
+  XMFLOAT2 Tex;
+};
+
+struct CBNeverChanges
+{
+  XMMATRIX mView;
+};
+
+struct CBChangeOnResize
+{
+  XMMATRIX mProjection;
+};
+
+struct CBChangesEveryFrame
+{
+  XMMATRIX mWorld;
+  XMFLOAT4 vMeshColor;
+};
